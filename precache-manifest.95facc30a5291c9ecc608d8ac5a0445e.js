e2806ad498542eac4937fdd2cf334c7a52aff7e3
Bipul2023 <bipulhaldar.india@gmail.com>
2023-05-21 22:09:18 +0530

    Updates

---
+++ precache-manifest.95facc30a5291c9ecc608d8ac5a0445e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb1b3a2f22017f5e0a3b",
+    "url": "/websitedevelopementtracker/static/css/main.956b8635.chunk.css"
+  },
+  {
+    "revision": "eb1b3a2f22017f5e0a3b",
+    "url": "/websitedevelopementtracker/static/js/main.eb1b3a2f.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/websitedevelopementtracker/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "6fc01341887df41ab6d3",
+    "url": "/websitedevelopementtracker/static/js/2.6fc01341.chunk.js"
+  },
+  {
+    "revision": "4c2de3e99c2698c98dde",
+    "url": "/websitedevelopementtracker/static/js/runtime~main.4c2de3e9.js"
+  },
+  {
+    "revision": "595771ee7ca2b558332d19377bc60ac9",
+    "url": "/websitedevelopementtracker/index.html"
+  }
+];
